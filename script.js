5772452b58eff8992782bc7c957d5352420b1683
luis <luiherra25@gmail.com>
2025-11-21 13:08:38 +0100

    solucion kata

--- script.js
+++ script.js
@@ -1,1 +1,22 @@
-console.log("Hola")
+let cargador = ["pium!","pium!","pium!","pium!","pium!","pium!","pium!"]
+function chauchat(parametro) {
+  let dejarEnfriar = 3
+  for (let i = 0; i < parametro.length; i++) {
+       let pillada = 0
+       posibilidadPillada = Math.floor(Math.random()*100)
+       console.log(posibilidadPillada)
+    if(posibilidadPillada < 80 && dejarEnfriar){
+      console.log("Illo me he quedao pillá!")
+      i = parametro.length
+    }  else if(!dejarEnfriar){
+      console.log("")
+      i--
+      dejarEnfriar = 3
+    } else { 
+      console.log("pium!")
+      dejarEnfriar--
+    }
+    // console.log("dejarEnfriar vale: ",dejarEnfriar)
+  }
+}
+chauchat(cargador)
